1ae8d2ddebbc10d4a5d73a06ecc4b2a6f8633abc
Nostradamused <nicolas.debengy@ynov.com>
2025-03-18 14:29:27 +0100

    feat(v0): only subtitles mssing from video, everything is be good otherwise

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import base64
+from enum import Enum
 from pydantic import BaseModel
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, Request, UploadFile, File, HTTPException
 
 
 app = FastAPI()
@@ -13,6 +14,14 @@ templates = Jinja2Templates(directory="app/templates")
 
 # Mount static files (for CSS, JS, images, etc.)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.middleware("http")
+async def add_security_headers(request, call_next):
+    response: Response = await call_next(request)
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    return response
 
 
 @app.get("/")
@@ -26,24 +35,23 @@ def read_root(request: Request):
 
 
 class FileData(BaseModel):
-    filename: str
-    content_type: str
+    translate: bool
     data: str  # On attend une chaîne Base64
 
 
 @app.post("/upload")
 def upload_file(file_data: FileData):
     """Reçoit un fichier encodé en Base64 et affiche ses infos."""
-
     # Décoder la base64 en bytes
     file_bytes = base64.b64decode(file_data.data)
 
     file_info = {
-        "filename": file_data.filename,
-        "content_type": file_data.content_type,
         "size": len(file_bytes),  # Taille du fichier en octets
     }
 
-    print(f"Fichier reçu: {file_info}")  # Debug
+    print(f"Fichier reçu: {file_info}")
 
-    return {"message": "Fichier reçu", "file_info": file_info}
+    with open("app/example/input.srt", "r") as f:
+        subtitles = f.read()
+
+    return {"subtitles": subtitles}
